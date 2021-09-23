a5912780d76076dc68a752566f6416d5ba506eb9
thakaj <taulant.hakaj@scranton.edu>
2021-09-23 17:53:04 -0400

    commit tree 4087d3da21829a1b66c597c0b6d06067fc47f48a

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.belongs_to :passengers
+      t.belongs_to :rides
 
       t.timestamps
     end
